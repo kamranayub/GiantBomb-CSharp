18adc4b8ef5eaf158aa973224a28cfcaa5f852db
Kamran Ayub <kayub@intrepidstudios.com>
2011-08-03 15:20:43 -0500

    Initial checkin of base RestSharp client

---
+++ GiantBomb.Api/Core.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using RestSharp;
+
+namespace GiantBomb.Api
+{
+    public partial class GiantBombRestClient
+    {
+        private RestClient _client;
+
+        /// <summary>
+        /// Base URL of API (defaults to http://api.giantbomb.com)
+        /// </summary>
+        public string BaseUrl { get; set; }
+
+        /// <summary>
+        /// Your GiantBomb API token
+        /// </summary>
+        private string ApiToken { get; set; }
+
+        public GiantBombRestClient(string apiToken)
+        {
+            BaseUrl = "https://api.giantbomb.com/";
+            ApiToken = apiToken;
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var assemblyName = new AssemblyName(assembly.FullName);
+            var version = assemblyName.Version;
+
+            _client = new RestClient();
+            _client.UserAgent = "giantbomb-csharp/" + version;
+            _client.BaseUrl = BaseUrl;
+
+            // API token is used on every request
+            _client.AddDefaultParameter("api_token", ApiToken, ParameterType.GetOrPost);
+        }
+
+#if FRAMEWORK
+        /// <summary>
+        /// Execute a manual REST request
+        /// </summary>
+        /// <typeparam name="T">The type of object to create and populate with the returned data.</typeparam>
+        /// <param name="request">The RestRequest to execute (will use client credentials)</param>
+        public T Execute<T>(RestRequest request) where T : new()
+        {
+        var response = _client.Execute<T>(request);
+        return response.Data;
+        }
+
+        /// <summary>
+        /// Execute a manual REST request
+        /// </summary>
+        /// <param name="request">The RestRequest to execute (will use client credentials)</param>
+        public RestResponse Execute(RestRequest request)
+        {
+        return _client.Execute(request);
+        }
+#endif
+    }
+}
---
+++ GiantBomb.Api/Games.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GiantBomb.Api.Model;
+using RestSharp;
+
+namespace GiantBomb.Api
+{
+    public partial class GiantBombRestClient
+    {
+
+        /// <summary>
+        /// Gets a game with the given ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Game GetGame(int id)
+        {
+            var request = new RestRequest
+            {
+                Resource = "game/{GameId}", 
+                RootElement = "results"
+            };
+
+            request.Method = Method.GET;
+            request.AddUrlSegment("GameId", id.ToString());
+
+            return Execute<Game>(request);
+        }
+    }
+}
