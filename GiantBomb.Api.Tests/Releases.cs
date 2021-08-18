d6d1fd4cefb337d0c5b914957ba3bd21c2742213
chyyran <ronny6993@gmail.com>
2021-08-18 18:22:25 -0400

    restsharp: Use System.Text.Json deserializer

--- GiantBomb.Api.Tests/Releases.cs
+++ GiantBomb.Api.Tests/Releases.cs
@@ -1,8 +1,6 @@
 ﻿using FluentAssertions;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Xunit;
 
 namespace GiantBomb.Api.Tests {
---
+++ GiantBomb.Api/Serialization/DateTimeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace GiantBomb.Api.Serialization
+{
+    internal class DateTimeConverter : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return DateTime.Parse(reader.GetString());
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+    }
+
+}
